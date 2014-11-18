8b1f23bebb714d2264537d6ad45aee9493f85e9e
James Baca <james.baca@ubnt.com>
2014-11-18 12:00:01 -0800

    Updating the app to use more current build versions

--- app/src/main/java/com/jimbaca/example/customviewgroup/MainActivity.java
+++ app/src/main/java/com/jimbaca/example/customviewgroup/MainActivity.java
@@ -1,14 +1,14 @@
 package com.jimbaca.example.customviewgroup;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 //import com.android.debug.hv.ViewServer;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
